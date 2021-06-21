db5eb58d5b8cd2c4e0fbcfb19263c149b064ca41
Dominik <77927514+OverRam@users.noreply.github.com>
2021-06-21 14:14:16 +0200

    Del isBuzz, refactor Explanation & main.

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -11,24 +11,18 @@ public class Main {
             System.out.println("This number is not natural!");
         } else {
             System.out.println(num % 2 == 0 ? "This number is Even." : "This number is Odd.");
-            System.out.printf("It is %sa Buzz number.\n", isBuzz(num) ? "" : "not ");
+            System.out.printf("It is %sa Buzz number.\n", num % 10 == 7 | num % 7 == 0 ? "" : "not ");
             Explanation(num);
         }
     }
 
-    static boolean isBuzz(int num) {
-        String s = num + "";
-        return s.charAt(s.length() - 1) == '7' | num / 7.0 == (num / 7);
-    }
-
     static void Explanation(int num) {
-        String s = num + "";
         System.out.print("Explanation\n" + num);
-        if (s.charAt(s.length() - 1) == '7' && num / 7.0 == (num / 7)) {
+        if (num % 10 == 7 && num % 7 == 0) {
             System.out.println(" is divisible by 7 and ends with 7");
-        } else if (s.charAt(s.length() - 1) == '7') {
+        } else if (num % 10 == 7) {
             System.out.println(" ends with 7");
-        } else if (num / 7.0 == (num / 7)) {
+        } else if (num % 7 == 0) {
             System.out.println(" is divisible by 7");
         } else {
             System.out.println(" is neither divisible by 7 nor does it end with 7");

a134d0f040028b1c30ff02183cc7fc905feb3346
Dominik <77927514+OverRam@users.noreply.github.com>
2021-06-27 22:34:10 +0200

    Implement method isSpy

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -2,13 +2,14 @@ package numbers;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         welcomeScreen();
-        long check = 2;
 
+        long check = 2;
         while (check != 0) {
             System.out.print("\nEnter a request: ");
             String[] strArr = sc.nextLine().split(" ");
@@ -16,6 +17,9 @@ public class Main {
             try {
                 for (int i = 0; i < strArr.length; i++) {
                     data[i] = Long.parseLong(strArr[i]);
+                    if (i == 2) {
+                        break;
+                    }
                 }
                 check = data[0];
 
@@ -44,23 +48,26 @@ public class Main {
     }
 
     static String requestForOne(long num) {
-        return String.format("Properties of %d\n%12s: %b\n%12s: %b\n%12s: %b\n%12s: %b\n%12s: %b\n%12s: %b", num,
-                "buzz", iSBuzz(num), "duck", isDuck(num), "palindromic", isPalindromic(num), "gapful", isGap(num),
-                "even", isEven(num), "odd", !isEven(num));
+        return String.format("Properties of %d\n%12s: %b\n%12s: %b\n%12s: %b\n%12s: %b\n%12s: %b\n%12s: %b\n%12s: %b",
+                num, "buzz", iSBuzz(num), "duck", isDuck(num), "palindromic", isPalindromic(num), "gapful", isGap(num),
+                "spy", isSpy(num), "even", isEven(num), "odd", !isEven(num));
     }
 
     static String requestForMulti(long num) {
-        return num + " is " + (iSBuzz(num) ? "buzz, " : "") + (isDuck(num) ? "duck," : "") + (isPalindromic(num)
-                ? "palindromic, " : "") + (isGap(num) ? "gapful, " : "") + (isEven(num) ? "even" : "odd");
+        return String.format("%20s", num + " is ") + (iSBuzz(num) ? "buzz, " : "") + (isDuck(num) ? "duck," : "") +
+                (isPalindromic(num) ? "palindromic, " : "") + (isGap(num) ? "gapful, " : "") +
+                (isSpy(num) ? "spy, " : "") + (isEven(num) ? "even" : "odd");
     }
 
     static void welcomeScreen() {
-        System.out.println("Welcome to Amazing Numbers!\n\n" +
+        System.out.println("Welcome to Amazing Numbers!\n" +
+                "\n" +
                 "Supported requests:\n" +
                 "- enter a natural number to know its properties;\n" +
                 "- enter two natural numbers to obtain the properties of the list:\n" +
                 "  * the first parameter represents a starting number;\n" +
-                "  * the second parameter shows how many consecutive numbers are to be printed;\n" +
+                "  * the second parameters show how many consecutive numbers are to be processed;\n" +
+                "- two natural numbers and a property to search for;\n" +
                 "- separate the parameters with one space;\n" +
                 "- enter 0 to exit.");
     }
@@ -80,6 +87,20 @@ public class Main {
         int firstLast = Integer.parseInt(gap.charAt(0) + "" + gap.charAt(gap.length() - 1));
 
         return !(gap.length() < 3 | num % firstLast != 0);
+    }
+
+    static boolean isSpy(long num) {
+        String[] numCharArr = Long.toString(num).split("");
+        long product = 1;
+        int sum = 0;
+        int dig;
+        for (String s : numCharArr) {
+            dig = Integer.parseInt(s);
+            sum += dig;
+            product *= dig;
+        }
+
+        return product == sum;
     }
 
     static boolean iSBuzz(long num) {

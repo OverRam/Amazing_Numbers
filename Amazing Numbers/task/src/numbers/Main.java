58fb4fc2a1c9ba643f30df8a2daff6fe544258c9
Dominik <77927514+OverRam@users.noreply.github.com>
2021-07-07 23:27:41 +0200

    refactor main method

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -8,84 +8,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         welcomeScreen();
-        long firstNumber = 1;
         String[] inpArr;
         int countNumbers;
         int countParam;
-        boolean propIsOn;
-        boolean isNotExc;
-        long[] numbersInp;
+        boolean propertyIsAvailable;
+        boolean isNotExclusive;
+        long[] numbers = {1};
 
-//        do {
+        do {
+            System.out.print("Enter a request: ");
+            inpArr = sc.nextLine().toUpperCase().trim().split(" ");
+            System.out.println();
 
-        UserParams uP = new UserParams();
+            String[] properties = {};
+            countNumbers = Check.CountNumbersInParams(inpArr);
+            countParam = inpArr.length - countNumbers;
+            propertyIsAvailable = true;
+            isNotExclusive = true;
 
+            if (countParam > 0 && countNumbers > 0) {
+                properties = Arrays.copyOfRange(inpArr, countNumbers, inpArr.length);
+                propertyIsAvailable = Check.checkAllProperties(properties);
+                isNotExclusive = !Check.exclusiveProperties(properties);
+            }
 
-//            System.out.print("Enter a request: ");
-//            inpArr = sc.nextLine().toUpperCase().split(" ");
-//            System.out.println();
-//
-//            String[] params;
-//            countNumbers = Check.CountNumbersInParams(inpArr);
-//            countParam = inpArr.length - countNumbers;
-//            propIsOn = true;
-//            isNotExc = true;
-//
-//            if (countParam > 0 && countNumbers > 0) {
-//                params = Arrays.copyOfRange(inpArr, countNumbers, inpArr.length);
-//                propIsOn = Check.checkAllProperties(params);
-//                isNotExc = !Check.exclusiveProperties(params);
-//            }
-//
-//            if (propIsOn && isNotExc && Check.numbers(inpArr)) {
-//                HandlerRequest.handlerRequest(inpArr, countNumbers, countParam);
-//
-//
-//                firstNumber = Long.parseLong(inpArr[0]);
-//
-//                if (firstNumber != 0) {
-//                    if (inpArr.length == 4) {
-//                        int findNext = 0;
-//                        int countNumber = 0;
-//
-//                        while (findNext < Long.parseLong(inpArr[1])) {
-//                            if (HandlerRequest.propertyMode(inpArr[2], firstNumber + countNumber) &&
-//                                    HandlerRequest.propertyMode(inpArr[3], firstNumber + countNumber)) {
-//
-//                                System.out.println(HandlerRequest.printRequestForMulti(firstNumber + countNumber));
-//                                findNext++;
-//                            }
-//                            countNumber++;
-//                        }
-//
-//                    } else if (countNumbers == 2 && countParam > 0) {
-//                        int findNext = 0;
-//                        int countNumber = 0;
-//
-//                        while (findNext < Long.parseLong(inpArr[1])) {
-//                            if (HandlerRequest.propertyMode(inpArr[2], firstNumber + countNumber)) {
-//                                System.out.println(HandlerRequest.printRequestForMulti(firstNumber + countNumber));
-//                                findNext++;
-//                            }
-//                            countNumber++;
-//                        }
-//                        System.out.println();
-//
-//                    } else if (countNumbers == 2) {
-//                        for (int i = 0; i < Long.parseLong(inpArr[1]); i++) {
-//                            System.out.println(HandlerRequest.printRequestForMulti(firstNumber + i));
-//                        }
-//                        System.out.println();
-//
-//                    } else {
-//                        System.out.println(HandlerRequest.printRequestForOne(firstNumber));
-//                    }
-//                }
-//            }
-//        } while (firstNumber != 0);
-//        System.out.println("Goodbye!");
-//    }
+            if (propertyIsAvailable && isNotExclusive && Check.numbers(inpArr)) {
+                numbers = Check.getNumbers(countNumbers, inpArr);
+                if (numbers[0] != 0) {
+                    HandlerRequest.print(numbers, properties, countNumbers, countParam);
+                }
+            }
+        } while (numbers[0] != 0);
+        System.out.println("Goodbye!");
     }
+
 
     private static void welcomeScreen() {
         System.out.println("Welcome to Amazing Numbers!\n" +

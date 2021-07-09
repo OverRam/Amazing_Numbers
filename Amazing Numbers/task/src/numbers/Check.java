554faa35b21284e3fcde945cbbcda61fce27480c
Dominik <77927514+OverRam@users.noreply.github.com>
2021-07-09 18:23:02 +0200

    Refactor method exclusiveProperties, adding check negative properties in badProperties method

--- Amazing Numbers/task/src/numbers/Check.java
+++ Amazing Numbers/task/src/numbers/Check.java
@@ -4,8 +4,7 @@ import java.util.InputMismatchException;
 
 public class Check {
     static final String[] propertiesArr = {"BUZZ", "DUCK", "PALINDROMIC", "GAPFUL", "SPY", "SQUARE", "SUNNY",
-            "JUMPING", "EVEN", "ODD"};
-    static final int propertiesLength = propertiesArr.length;
+            "JUMPING", "HAPPY", "SAD", "EVEN", "ODD"};
 
     static boolean numbers(String[] arr) {
         int i = 0;
@@ -35,6 +34,9 @@ public class Check {
 
     private static boolean badProperties(String inp) {
         for (String s : propertiesArr) {
+            if (inp.charAt(0) == '-') {
+                inp = inp.replaceFirst("-", "");
+            }
             if (inp.equals(s)) {
                 return true;
             }
@@ -79,46 +81,28 @@ public class Check {
     }
 
     static boolean exclusiveProperties(String[] params) {
-        int even = 0;
-        int odd = 0;
-        int duck = 0;
-        int spy = 0;
-        int sunny = 0;
-        int square = 0;
+        boolean isExc = false;
         StringBuilder sb = new StringBuilder();
 
-        for (String e : params) {
-
-            if (e.equals("EVEN") || e.equals("ODD")) {
-                even += e.equals("EVEN") && even < 1 ? 1 : 0;
-                odd += e.equals("ODD") && odd < 1 ? 1 : 0;
-                if (even + odd >= 2) {
-                    sb.append("EVEN, ODD");
-                    break;
+        for (int i = 0; !isExc && i < params.length; i++) {
+            String e = params[i];
+            for (int j = 0; j < params.length; j++) {
+                String s = params[j];
+                if (i != j) {
+                    //find pattern: x && -x || -x && x  (example even && -even)
+                    if (e.equals("EVEN") && s.equals("ODD") || e.equals("-EVEN") && s.equals("-ODD") ||
+                            e.equals("HAPPY") && s.equals("SAD") || e.equals("-HAPPY") && s.equals("-SAD") ||
+                            (e.contains("DUCK") && s.contains("SPY") || e.contains("SUNNY") && s.contains("SQUARE"))
+                                    && !e.contains("-") && !s.contains("-") || (e.equals("-" + s) ||
+                            ("-" + e).equals(s))) {
+                        isExc = true;
+                        sb.append(e).append(", ").append(s);
+                        break;
+                    }
                 }
-            }
 
-            if (e.equals("DUCK") || e.equals("SPY")) {
-                duck += e.equals("DUCK") && duck < 1 ? 1 : 0;
-                spy += e.equals("SPY") && spy < 1 ? 1 : 0;
-                if (duck + spy >= 2) {
-                    sb.append("DUCK, SPY");
-                    break;
-                }
-            }
-
-            if (e.equals("SUNNY") || e.equals("SQUARE")) {
-                sunny += e.equals("SUNNY") && sunny < 1 ? 1 : 0;
-                square += e.equals("SQUARE") && square < 1 ? 1 : 0;
-                if (sunny + square >= 2) {
-                    sb.append("SUNNY, SQUARE");
-                    break;
-                }
             }
         }
-
-        boolean isExc = even + odd >= 2 || duck + spy >= 2 || sunny + square >= 2;
-
         if (isExc) {
             Errors.exclusiveProperties(sb.toString());
         }
@@ -133,5 +117,3 @@ public class Check {
         return numbers;
     }
 }
-
-
